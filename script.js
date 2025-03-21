9207aaea70490ca71cc28f919cf39e5f46b8a7c7
medinanzz <apololick2@gmail.com>
2025-03-20 21:43:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function button() {
+    Swal.fire ({
+        text: 'As receitas deste site, 35% foram feitas por IA!',
+        icon: 'info',
+        draggable: true,
+        showConfirmButton: false,
+        timer: '2000',
+        footer: '<a href="https://chatgpt.com/">Ideias tiradas daqui!</a>'
+    })
+}
